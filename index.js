dbe9e3c0b2d07f78670c5dec62b08aacdb98e8ca
pawel <pawel.dabrowski@unifiedfactory.com>
2017-09-15 16:53:48 +0200

    db connect

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const MongoClient = require('mongodb').MongoClient;
 const io = require('socket.io')(http);
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
 });
 
-io.on('connection', function (socket) {
+MongoClient.connect('mongodb://doge_db_admin_1:databasePasswordQ!@#@ds135574.mlab.com:35574/doge_db_1', (err, db) => {
+    if (err) throw err;
+
+    db.collection('mammals').find().toArray((err, result) => {
+        if (err) throw err;
+
+        console.log(result);
+    })
+});
+
+io.on('connection', (socket) => {
     console.log('a user connected');
 });
 
-http.listen(8080, function () {
-    console.log('listening on *:3000');
+http.listen(8080, () => {
+    console.log('listening on *:8080');
 });
